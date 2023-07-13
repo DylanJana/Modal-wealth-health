4eb131c919f4e31a6da4fbd2a27b078e2185d86e
Dylan Durand <dylan.durand88@outlook.fr>
2023-07-13 12:37:44 +0200

    Update plugin 13072023

---
+++ dist/Modal/ModalDylan.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import './Modal.css';
+export default function Modal({
+  children,
+  formIsSuccess = true,
+  onClose = () => {},
+  onClick = () => {}
+}) {
+  const [modalIsOpen, setModalIsOpen] = useState(formIsSuccess);
+  useEffect(() => {
+    setModalIsOpen(formIsSuccess);
+    let body = document.querySelector('body');
+    formIsSuccess === true ? body.classList.add('no--scroll') : body.classList.remove('no--scroll');
+  }, [formIsSuccess]);
+  const handleClose = () => {
+    setModalIsOpen(false);
+    onClose();
+  };
+  const resetForm = () => {
+    onClick();
+  };
+  return /*#__PURE__*/React.createElement("div", {
+    className: "modal-container"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "modal-content"
+  }, /*#__PURE__*/React.createElement("p", null, children), /*#__PURE__*/React.createElement("div", {
+    className: "modal-content__ctas"
+  }, /*#__PURE__*/React.createElement("button", {
+    className: "btn--plain btn--green",
+    onClick: handleClose
+  }, "Retourner au tableau"), /*#__PURE__*/React.createElement("button", {
+    className: "btn--plain btn--green",
+    onClick: resetForm
+  }, "Ajouter un nouvel employ\xE9"))));
+}
---
+++ dist/TestModal/TestModal.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ModalDylan from '../Modal/ModalDylan';
+export default function TestModal() {
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement(ModalDylan, null));
+}
